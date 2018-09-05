ec1ec6fe7e6bb1ad7e680b5bcb1bf61c0d35d058
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 16:43:01 -0700

    Changed integer check to use less resources.

--- game.py
+++ game.py
@@ -9,13 +9,15 @@ name = input("What's your name? ")
 
 random_n = random.randrange(1,101)
 
+
+choice = input("I picked a number from 1 to 100. Cat you guess it? ")
+
+
 guesses = 1
 
 while True:
 
-    choice = input("I picked a number from 1 to 100. Cat you guess it? ")
-
-    while not ((choice.isdigit() == True) and (int(choice) in range (1,101))):
+    while not ((choice.isdigit() == True) and (int(choice)>0) and (int(choice)<101)):
         choice = input("Incorrect input. Pick a number between 1 and 100. ")
 
     if int(choice) == random_n:
