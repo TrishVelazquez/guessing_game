d0dd3d53505f5f2031aa80134cd0d18e66563434
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 13:05:06 -0700

    Added random number. Asked player for guess. Added while loop to check value.

--- game.py
+++ game.py
@@ -1,6 +1,24 @@
 """A number-guessing game."""
 
 # Put your code here
+import random
+
 print("Hi player!")
 
 name = input("What's your name? ")
+
+random_n = random.randrange(1,101)
+
+choice = input("I picked a number from 1 to 100. Cat you guess it? ")
+
+guesses = 1
+
+while True:
+    if int(choice) == random_n:
+        print("You won! You had {} tries".format(guesses))
+        break
+
+    if int(choice) != random_n:
+        choice = input("Guess again! ")
+
+
