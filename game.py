1addf8d6827144a0faf4c83e2fb6de04070ea300
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 17:56:58 -0700

    Fixed guess counter

--- game.py
+++ game.py
@@ -3,31 +3,49 @@
 # Put your code here
 import random
 
+best_score = None
+
 print("Hi player!")
 
 name = input("What's your name? ")
 
-random_n = random.randrange(1,101)
+while  True:
+
+    random_n = random.randrange(1,101)
 
 
-choice = input("I picked a number from 1 to 100. Cat you guess it? ")
+    choice = input("I picked a number from 1 to 100. Can you guess it? ")
 
 
-guesses = 1
+    guesses = 1
 
-while True:
+    while True:
 
-    while not ((choice.isdigit() == True) and (int(choice)>0) and (int(choice)<101)):
-        choice = input("Incorrect input. Pick a number between 1 and 100. ")
+        while not ((choice.isdigit() == True) and (int(choice)>0) and (int(choice)<101)):
+            choice = input("Incorrect input. Pick a number between 1 and 100. ")
 
-    if int(choice) == random_n:
-        print("You won! You had {} tries".format(guesses))
+
+        if int(choice) == random_n:
+            if best_score == None:
+                print("You won! You had {} tries!".format(guesses))
+                best_score = guesses
+                break
+            else:
+                if best_score > guesses:
+                    best_score = guesses
+                print("You won! You had {} tries and your best score is {}!".format(guesses,best_score))
+                break
+
+        if int(choice) > random_n:
+            choice = input("Too big! Guess again! ")
+            guesses+=1
+        else:
+            choice = input("Too small! Guess again! ")
+            guesses+=1
+
+
+    answer = input("Do you want to play again? (Y/N) ")
+
+    if answer == 'N':
         break
-
-    if int(choice) > random_n:
-        choice = input("Too big! Guess again! ")
-        guesses+=1
-    else:
-        choice = input("Too small! Guess again! ")
-        guesses+=1
  
